ca69f0b0898abbedb4c3060b8b0e530c65379e53
ajalapraise <akintomiisin@gmail.com>
2023-10-12 15:23:04 +0100

    First commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
+import Layout from '@/components/layout/layout';
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Montserrat } from "next/font/google";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const montserrat = Montserrat({
+  variable: "--Montserrat",
+  subsets: ["latin"],
+  display: "swap",
+  weight: "100",
+});
+
+export default function App({
+  Component,
+  pageProps }: AppProps) {
+  return (
+    <Layout>
+      <main className={`bg-white text-black ${montserrat.className} `}>
+        <Component {...pageProps} />
+      </main>
+    </Layout>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,46 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "888px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    fontSize: {
+      base: "14px",
+      sm: "18px",
+      md: "20px",
+      lg: "24px",
+      xl: "32px",
+      "2xl": "40px",
+    },
+    colors: {
+      white: '#FFFFFF',
+      black: '#101828',
+      blue: '#175CD3',
+      grey: '#475467',
+      grey1: '#667085',
+      grey3: '#F2F4F7',
+      grey4: '#D0D5DD',
+      blue2: '#528BFF',
+      blue3: '#004EEB',
+      transparent: 'transparent',
+
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
